11c4a355116fce3d00439975b7e6e625cf1c1731
Jesus Magana <maganazero5@gmail.com>
2014-08-18 17:51:51 -0700

    Added 3rd kinetic equation

--- newtonianMechanics.cpp
+++ newtonianMechanics.cpp
@@ -13,11 +13,18 @@ double findDistVVT(double initVel, double finalVel, double time)
 	return ((initVel + finalVel) / 2 * time);
 }
 
+double findVelVAD(double initVel, double accel, double dist)
+{
+	return (sqrt(pow(initVel,2) + 2 * accel * dist));
+}
+
 int main()
 {
 	int initVel, accel, times, finalVel;
 	cin >> initVel >> accel >> times >> finalVel;
-	cout << findDistVAT(initVel, accel, times) << endl;
-	cout << findDistVVT(initVel, finalVel, times) << endl;
+	double dist = findDistVAT(initVel, accel, times);
+	cout << "Distance = " << dist << endl;
+	cout << "Distance = " << findDistVVT(initVel, finalVel, times) << endl;
+	cout << "Velocity = " << findVelVAD(initVel, accel, dist) << endl;
 	return 0;
 }
