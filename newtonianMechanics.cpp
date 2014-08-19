d08559e0b3e0fec27d47e8b3dfe7cfad2c2e0ca7
Jesus Magana <maganazero5@gmail.com>
2014-08-18 17:32:34 -0700

    first kinematic equation works

--- newtonianMechanics.cpp
+++ newtonianMechanics.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
+double findDist(double velocity, double accel, double time)
+{
+	return (velocity * time  + 0.5 * accel * pow(time,2));
+}
+
 int main()
 {
-	cout << "Physics start" << endl;
+	int testInput1, testInput2, testInput3;
+	cin >> testInput1 >> testInput2 >> testInput3;
+	cout << findDist(testInput1, testInput2, testInput3) << endl;
 	return 0;
 }
