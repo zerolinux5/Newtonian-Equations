44d37b7ff853ce2597d824c74ff589a8dcda76a4
Jesus Magana <maganazero5@gmail.com>
2014-09-28 15:53:43 -0700

    Added test program to make sure classes work

--- supplimentaryFiles.cpp
+++ supplimentaryFiles.cpp
@@ -5,12 +5,12 @@ class Point {
    protected:
 	double x, y;
    public:
-	void set_values(double,double);
-	double get_x() {return x;};
-	double get_y() {return y;};
+	void setValues(double,double);
+	double getX() {return x;};
+	double getY() {return y;};
 };	
 
-void Point::set_values (double inX, double inY) {
+void Point::setValues (double inX, double inY) {
 	x = inX;
 	y = inY;
 }
@@ -18,11 +18,11 @@ void Point::set_values (double inX, double inY) {
 class PointMass: public Point {
 	double mass;
    public:
-	void set_values(double, double, double);
-	double get_mass(){ return mass;}
+	void setValues(double, double, double);
+	double getMass(){ return mass;}
 };
 
-void PointMass::set_values(double inMass, double inX, double inY){
+void PointMass::setValues(double inMass, double inX, double inY){
 	mass = inMass;
 	x = inX;
 	y = inY;
@@ -51,6 +51,12 @@ void PointMass::set_values (double inMass, double inX, double inY){
 
 int main()
 {
-	cout << "Runs" << endl;
+	Point p1;
+	p1.setValues(2,3);
+	cout << "Point 1: " << p1.getX() << "," << p1.getY() << endl;
+
+	PointMass p2;
+	p2.setValues(100,3,4);
+	cout << "Point 2: " << p2.getX() << "," << p2.getY() << " mass:" << p2.getMass() << endl;
 	return 0;
 }
